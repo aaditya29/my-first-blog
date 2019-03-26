0ace9da5009d26bee5b6ace99f8f6ffd5c44ef08
Aditya Mishra <mishraaaditya29@gmail.com>
2019-03-26 07:12:53 +0530

    Modified Templates

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,12 @@
 #It will request information from the model we created before and pass it to a template.
 
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post#The . means current directory or current application
 
 # Create your views here.
 def post_list(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {})
 
 #Here we greated a function called post_list that takes request and will return the value
